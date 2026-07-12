8d16d47c417a87c5a0ea85d9207a33306001e3c6
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:40 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "postgresql_configurations" {
-  description = "All postgresql_configuration resources"
-  value       = azurerm_postgresql_configuration.postgresql_configurations
-}
 output "postgresql_configurations_name" {
-  description = "List of name values across all postgresql_configurations"
-  value       = [for k, v in azurerm_postgresql_configuration.postgresql_configurations : v.name]
+  description = "Map of name values across all postgresql_configurations, keyed the same as var.postgresql_configurations"
+  value       = { for k, v in azurerm_postgresql_configuration.postgresql_configurations : k => v.name }
 }
 output "postgresql_configurations_resource_group_name" {
-  description = "List of resource_group_name values across all postgresql_configurations"
-  value       = [for k, v in azurerm_postgresql_configuration.postgresql_configurations : v.resource_group_name]
+  description = "Map of resource_group_name values across all postgresql_configurations, keyed the same as var.postgresql_configurations"
+  value       = { for k, v in azurerm_postgresql_configuration.postgresql_configurations : k => v.resource_group_name }
 }
 output "postgresql_configurations_server_name" {
-  description = "List of server_name values across all postgresql_configurations"
-  value       = [for k, v in azurerm_postgresql_configuration.postgresql_configurations : v.server_name]
+  description = "Map of server_name values across all postgresql_configurations, keyed the same as var.postgresql_configurations"
+  value       = { for k, v in azurerm_postgresql_configuration.postgresql_configurations : k => v.server_name }
 }
 output "postgresql_configurations_value" {
-  description = "List of value values across all postgresql_configurations"
-  value       = [for k, v in azurerm_postgresql_configuration.postgresql_configurations : v.value]
+  description = "Map of value values across all postgresql_configurations, keyed the same as var.postgresql_configurations"
+  value       = { for k, v in azurerm_postgresql_configuration.postgresql_configurations : k => v.value }
 }
 
